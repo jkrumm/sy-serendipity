41b426b28c3c55421b29991b2de996d71c6cef3e
jkrumm <jkrumm@pm.me>
2024-07-21 21:00:36 +0200

    fixes mobile video autoplay on full screen

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import * as styles from './hero.module.scss';
 import useWindowDimensions from '../util/window-dimensions';
 import { getImg } from '../util/get-image';
 
 function Hero() {
   const { height, width } = useWindowDimensions('fix');
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+
+    const handlePlay = () => {
+      video.play();
+    };
+
+    // Add event listeners for click and touch events
+    video.addEventListener('click', handlePlay);
+    video.addEventListener('touchstart', handlePlay);
+
+    // Cleanup event listeners on unmount
+    return () => {
+      video.removeEventListener('click', handlePlay);
+      video.removeEventListener('touchstart', handlePlay);
+    };
+  }, []);
+
   return (
     <div className={styles.hero}>
       <div className={styles.imageWrapper}>
         {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-        <video width={width} height={height} autoPlay muted>
+        <video
+          width={width}
+          height={height}
+          ref={videoRef}
+          playsInline // Prevent fullscreen on mobiles
+          controls // Allows user to interact with play button
+          autoPlay
+          muted
+        >
           <source
             src="https://ik.imagekit.io/bgmwrkfoi/sy-serendipty-1080.mov?updatedAt=1717936116217"
             type="video/mp4"
